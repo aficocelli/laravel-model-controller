7280273ea476503bfd69cfd4894f98f4ce74d60d
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-05-14 16:45:58 +0200

    add. html /css on index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,20 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    @foreach ($songs as $song)
-        
-    <div class="box">
+    <h1>Playlist</h1>
+    <section class="playlist">
+        @foreach ($songs as $song)
+            
+        <div class="box">
+            <div class="cover">
+                <img src="{{$song -> cover}}" alt="">
+            </div>
+            <h2>{{$song -> title}}</h2>
+            <h3>{{$song -> author}}</h3>
+            <p>{{$song -> genre}}</p>
+            <p>{{$song -> released_year}}</p>
 
-        <div class="cover">
-            <img src="{{$song -> cover}}" alt="">
+
         </div>
-        <h2>{{$song -> title}}</h2>
-
-
-    </div>
-   @endforeach
-
+    @endforeach
+    </section>
 </body>
 </html>
