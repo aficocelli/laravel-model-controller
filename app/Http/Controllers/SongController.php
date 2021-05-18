37262ba4bb60611f5b5c8390edc4a720e6f14fcb
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-05-18 15:35:48 +0200

    fix. function show

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -48,10 +48,9 @@ class SongController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Song $song)
     {
-        $song = Song::find($id);
-
+    
         return view('songs.show', [
             'song'=>$song
         ]);
